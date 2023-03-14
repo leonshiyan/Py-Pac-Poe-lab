9c9de232834e123552d9fe0b2d05d054cde7e37a
leonshiyan <leonshiyan@gmail.com>
2023-03-13 21:37:37 -0700

    board set up

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -2,22 +2,14 @@
 board = {}
 turn = 'X'
 
-# Will not work
-def init_game():
-	# Will not work because this creates a new variable
-	# instead of assigning to the global board variable
-	board = {
-		'a1': None, `b1`: None, 'c1' None,
-		# etc
-	}
-	turn = 'X'
    
 # Do it like this
 def init_game():
 	# Use the global keyword to update global variables
 	global board, turn
 	board = {
-		'a1': None, `b1`: None, 'c1' None,
-		# etc
+		'a1': None, 'b1': None, 'c1' : None,
+		'a2': None, 'b2': None, 'c2' : None,
+		'a3': None, 'b3': None, 'c3' : None,
 	}
 	turn = 'X'
